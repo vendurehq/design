1687df25560ddd60587b962e0efbd0ce01f7a1b1
David Höck <d.hoeck@vendure.io>
2026-03-26 10:14:32 +0100

    fix(design-tokens): use 0.2rem for border radii

--- packages/design-tokens/src/tokens/radii.ts
+++ packages/design-tokens/src/tokens/radii.ts
@@ -1,11 +1,11 @@
 export const radii = {
   none: '0',
-  sm: '0.125rem',
-  md: '0.125rem',
-  lg: '0.125rem',
-  xl: '0.125rem',
-  '2xl': '0.125rem',
-  '3xl': '0.125rem',
-  '4xl': '0.125rem',
+  sm: '0.2rem',
+  md: '0.2rem',
+  lg: '0.2rem',
+  xl: '0.2rem',
+  '2xl': '0.2rem',
+  '3xl': '0.2rem',
+  '4xl': '0.2rem',
   full: '9999px',
 } as const;
